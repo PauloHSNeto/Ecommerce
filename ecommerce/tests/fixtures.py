075ed85dc4c140dd04c9e4861d3125f870a0382a
neto.564 <2100098@aluno.univesp.br>
2022-01-20 16:57:07 -0300

    new

--- ecommerce/tests/fixtures.py
+++ ecommerce/tests/fixtures.py
@@ -1,5 +1,6 @@
 import pytest
 from django.contrib.auth.models import User
+from django.core.management import call_command
 
 
 
@@ -9,3 +10,15 @@ def create_admin_user(django_user_model):
     Return admin user
     """
     return  django_user_model.objects.create_superuser("paulo","a@a.com","senha")
+
+
+
+
+@pytest.fixture(scope ="session")
+def django_db_setup(django_db_blocker):
+        """
+        Load DB data Fixtures
+        
+       """
+   with django_db_blocker.unblock():
+        call_command("loaddata","db_admin_fixture.json")  
