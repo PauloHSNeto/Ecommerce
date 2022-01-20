bdd8af1369ad0d57f5ceda210dbaebd0add6c895
neto.564 <2100098@aluno.univesp.br>
2022-01-20 16:21:51 -0300

    password

--- ecommerce/dashboard/tests/test_selenium_dashboard.py
+++ ecommerce/dashboard/tests/test_selenium_dashboard.py
@@ -17,10 +17,10 @@ def  test_dashboard_admin_login(live_server,create_admin_user,chrome_browser_ins
 
     user_name = browser.find_element(By.NAME,"username")
     user_password = browser.find_element(By.NAME,"password")
-    submit = browser.find_element(By.XPATH, '//imput[@value="Log in"]')
+    submit = browser.find_element(By.XPATH,'//input[@value="Log in"]')
 
-    user_name.send_keys("paulo")
-    user_password.send_keys("paulo")
+    user_name.send_keys("admin")
+    user_password.send_keys("password")
     submit.send_keys(Keys.RETURN)
 
     assert "Site administration" in browser.page_source
